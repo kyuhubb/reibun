ae49af97c487cefcc898f1a2af6621d45508dd7b
Kyuchama <35692364+kyuchama@users.noreply.github.com>
2021-11-17 01:03:08 +0100

    Add files via upload

--- db/credits.js
+++ db/credits.js
@@ -2,11 +2,15 @@ function getC(credit){
 	switch(credit){
 		case '1_001':
 		link = "https://www.amazon.co.jp/%E9%AD%94%E5%A5%B3%E3%81%AE%E5%AE%B6-%E3%82%A8%E3%83%AC%E3%83%B3%E3%81%AE%E6%97%A5%E8%A8%98-%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9%E3%82%A8%E3%82%A4%E3%82%B8-%E5%BD%B1%E5%B4%8E-%E7%94%B1%E9%82%A3-ebook/dp/B077S47L7V";
-		return "魔女の家 | 第一巻";
+		return "魔女の家 エレンの日記 | 第一巻";
 		break
 		case '2_001':
 		link = "https://www.amazon.co.jp/%E2%97%8B%C3%97%E2%96%B3%E3%81%B9%E3%83%BC%E3%81%99-1-%E3%81%AD%E3%81%A3%E3%81%A8%E3%82%8A%E3%81%90%E3%81%A1%E3%82%83%E3%81%90%E3%81%A1%E3%82%83%E3%82%BB%E3%83%AB%E3%83%AD%E3%82%A4%E3%83%89-%E3%83%95%E3%82%A1%E3%83%9F%E9%80%9A%E6%96%87%E5%BA%AB-%E6%9C%88%E6%9C%AC%E4%B8%80/dp/404727027X";
 		return "○X△べーす | 第一巻";
+		break
+		case '2_002':
+		link = "https://www.amazon.co.jp/%E3%83%80%E3%83%B3%E3%82%AC%E3%83%B3%E3%83%AD%E3%83%B3%E3%83%91-%E3%82%BC%E3%83%AD-%E6%98%9F%E6%B5%B7%E7%A4%BEFICTIONS-%E5%B0%8F%E9%AB%98-%E5%92%8C%E5%89%9B/dp/4061388126";
+		return "ダンガンロンパ/ゼロ| 第一巻(上)";
 		break
 	}
 }
