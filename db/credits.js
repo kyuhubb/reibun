db140bc4465f82ec3e9c0f1647b7332f5148425d
Kyuchama <35692364+kyuchama@users.noreply.github.com>
2021-12-25 21:28:55 +0100

    Add files via upload

--- db/credits.js
+++ db/credits.js
@@ -2,12 +2,17 @@ function getC(credit){
 	switch(credit){
 		case '1_001':
 		link = "https://www.amazon.co.jp/%E9%AD%94%E5%A5%B3%E3%81%AE%E5%AE%B6-%E3%82%A8%E3%83%AC%E3%83%B3%E3%81%AE%E6%97%A5%E8%A8%98-%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9%E3%82%A8%E3%82%A4%E3%82%B8-%E5%BD%B1%E5%B4%8E-%E7%94%B1%E9%82%A3-ebook/dp/B077S47L7V";
-		return "魔女の家 エレンの日記 | 第一巻";
+		return "魔女の家 エレンの日記 | 第一巻(漫画版)";
 		break
 
 		case '1_002':
 		link = "https://www.amazon.co.jp/%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%81%AF%E7%AC%91%E3%82%8F%E3%81%AA%E3%81%84-%E9%9B%BB%E6%92%83%E6%96%87%E5%BA%AB-%E4%B8%8A%E9%81%A0%E9%87%8E-%E6%B5%A9%E5%B9%B3-ebook/dp/B00QWGZ1JG/ref=sr_1_2?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=3M6W3GEXZ5GYI&keywords=%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%81%AF%E7%AC%91%E3%82%8F%E3%81%AA%E3%81%84&qid=1637627641&sprefix=%E3%81%B6%E3%81%8E%E3%83%BC%2Caps%2C321&sr=8-2";
 		return "ブギーポップは笑わない";
+		break
+
+		case '1_003':
+		link = "https://www.amazon.co.jp/gp/product/B00FZH3NR0?ref_=dbs_m_nmg_rwt_calw_tkin_0&storeType=ebooks&qid=1640463159&sr=8-1";
+		return "この素晴らしい世界に祝福を！ | 第一巻";
 		break
 
 		case '2_001':
