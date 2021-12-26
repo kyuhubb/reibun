096e10312831da1dbc40ab2df98a0558c1b0a726
Kyuchama <35692364+kyuchama@users.noreply.github.com>
2021-12-26 23:43:09 +0100

    Add files via upload

--- db/credits.js
+++ db/credits.js
@@ -1,47 +1,52 @@
 function getC(credit){
 	switch(credit){
 		case '1_001':
-		link = "https://www.amazon.co.jp/%E9%AD%94%E5%A5%B3%E3%81%AE%E5%AE%B6-%E3%82%A8%E3%83%AC%E3%83%B3%E3%81%AE%E6%97%A5%E8%A8%98-%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%82%B3%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9%E3%82%A8%E3%82%A4%E3%82%B8-%E5%BD%B1%E5%B4%8E-%E7%94%B1%E9%82%A3-ebook/dp/B077S47L7V";
+		link = "https://www.amazon.co.jp/gp/product/B077S47L7V";
 		return "魔女の家 エレンの日記 | 第一巻(漫画版)";
 		break
 
 		case '1_002':
-		link = "https://www.amazon.co.jp/%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%81%AF%E7%AC%91%E3%82%8F%E3%81%AA%E3%81%84-%E9%9B%BB%E6%92%83%E6%96%87%E5%BA%AB-%E4%B8%8A%E9%81%A0%E9%87%8E-%E6%B5%A9%E5%B9%B3-ebook/dp/B00QWGZ1JG/ref=sr_1_2?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=3M6W3GEXZ5GYI&keywords=%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%81%AF%E7%AC%91%E3%82%8F%E3%81%AA%E3%81%84&qid=1637627641&sprefix=%E3%81%B6%E3%81%8E%E3%83%BC%2Caps%2C321&sr=8-2";
+		link = "https://www.amazon.co.jp/gp/product/B00QWGZ1JG";
 		return "ブギーポップは笑わない";
 		break
 
 		case '1_003':
-		link = "https://www.amazon.co.jp/gp/product/B00FZH3NR0?ref_=dbs_m_nmg_rwt_calw_tkin_0&storeType=ebooks&qid=1640463159&sr=8-1";
+		link = "https://www.amazon.co.jp/gp/product/B00FZH3NR0";
 		return "この素晴らしい世界に祝福を！ | 第一巻";
 		break
 
+		case '1_004':
+		link = "https://www.amazon.co.jp/gp/product/B00GZK5SCY";
+		return "この素晴らしい世界に祝福を！ | 第二巻";
+		break
+
 		case '2_001':
-		link = "https://www.amazon.co.jp/%E3%83%80%E3%83%B3%E3%82%AC%E3%83%B3%E3%83%AD%E3%83%B3%E3%83%91-%E3%82%BC%E3%83%AD-%E6%98%9F%E6%B5%B7%E7%A4%BEFICTIONS-%E5%B0%8F%E9%AB%98-%E5%92%8C%E5%89%9B/dp/4061388126";
+		link = "https://www.amazon.co.jp/gp/product/4061388126";
 		return "ダンガンロンパ/ゼロ| 第一巻(上)";
 		break
 
 		case '2_002':
-		link = "https://www.amazon.co.jp/%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%83%BB%E3%83%AA%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%BA-VS%E3%82%A4%E3%83%9E%E3%82%B8%E3%83%8D%E3%83%BC%E3%82%BF%E3%83%BCPart1-%E9%9B%BB%E6%92%83%E6%96%87%E5%BA%AB-%E4%B8%8A%E9%81%A0%E9%87%8E-%E6%B5%A9%E5%B9%B3-ebook/dp/B00QWGYZY8/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%83%BB%E3%83%AA%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%BA+VS%E3%82%A4%E3%83%9E%E3%82%B8%E3%83%8D%E3%83%BC%E3%82%BF%E3%83%BCPart1&qid=1637629547&sr=8-1";
+		link = "https://www.amazon.co.jp/gp/product/B00QWGYZY8";
 		return "ブギーポップ・リターンズ　VS　イマジネーターPart1";
 		break
 
 		case '3_001':
-		link = "https://www.amazon.co.jp/%E5%A4%9C%E6%98%8E%E3%81%91%E3%81%AE%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97-DENGEKI-%E4%B8%8A%E9%81%A0%E9%87%8E-%E6%B5%A9%E5%B9%B3/dp/404912274X/ref=sr_1_3?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E5%A4%9C%E6%98%8E%E3%81%91%E3%81%AE%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97&qid=1637629269&sr=8-3";
+		link = "https://www.amazon.co.jp/gp/product/404912274X";
 		return "夜明けのブギーポップ";
 		break
 
 		case '3_002':
-		link = "https://www.amazon.co.jp/%E2%97%8B%C3%97%E2%96%B3%E3%81%B9%E3%83%BC%E3%81%99-1-%E3%81%AD%E3%81%A3%E3%81%A8%E3%82%8A%E3%81%90%E3%81%A1%E3%82%83%E3%81%90%E3%81%A1%E3%82%83%E3%82%BB%E3%83%AB%E3%83%AD%E3%82%A4%E3%83%89-%E3%83%95%E3%82%A1%E3%83%9F%E9%80%9A%E6%96%87%E5%BA%AB-%E6%9C%88%E6%9C%AC%E4%B8%80/dp/404727027X";
+		link = "https://www.amazon.co.jp/gp/product/404727027X";
 		return "○X△べーす | 第一巻";
 		break
 
 		case '3_003':
-		link = "https://www.amazon.co.jp/%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%83%BB%E3%83%AA%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%BA-VS%E3%82%A4%E3%83%9E%E3%82%B8%E3%83%8D%E3%83%BC%E3%82%BF%E3%83%BCPart2-%E9%9B%BB%E6%92%83%E6%96%87%E5%BA%AB-%E4%B8%8A%E9%81%A0%E9%87%8E-%E6%B5%A9%E5%B9%B3-ebook/dp/B00QWGZ0AG/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E3%83%96%E3%82%AE%E3%83%BC%E3%83%9D%E3%83%83%E3%83%97%E3%83%BB%E3%83%AA%E3%82%BF%E3%83%BC%E3%83%B3%E3%82%BA+VS%E3%82%A4%E3%83%9E%E3%82%B8%E3%83%8D%E3%83%BC%E3%82%BF%E3%83%BCPart2&qid=1637629609&sr=8-1";
+		link = "https://www.amazon.co.jp/gp/product/B00QWGZ0AG";
 		return "ブギーポップ・リターンズ VS　イマジネーターPart2";
 		break
 
 		case '3_004':
-		link = "https://www.amazon.co.jp/%E9%AD%94%E5%A5%B3%E3%81%AE%E6%97%85%E3%80%85-GA%E3%83%8E%E3%83%99%E3%83%AB-%E7%99%BD%E7%9F%B3-%E5%AE%9A%E8%A6%8F-ebook/dp/B01DK0TRFK/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&keywords=%E9%AD%94%E5%A5%B3%E3%81%AE%E6%97%85%E3%80%85+%28GA%E3%83%8E%E3%83%99%E3%83%AB%29&qid=1639263665&s=books&sr=1-1";
+		link = "https://www.amazon.co.jp/gp/product/B01DK0TRFK";
 		return "魔女の旅々(GAノベル) | 第一巻";
 		break
 	}
