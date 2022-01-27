88327d5e2c8bffa350020172db484df5610e30f4
Kyu <35692364+budouen@users.noreply.github.com>
2022-01-27 03:10:21 +0100

    Add files via upload

--- db/credits.js
+++ db/credits.js
@@ -6,58 +6,73 @@ function getC(credit){
 		break
 
 		case '1_002':
+		link = "https://www.amazon.co.jp/dp/B0848V5LGR/";
+		return "星の子(朝日文庫)";
+		break
+
+		case '1_003':
+		link = "https://www.amazon.co.jp/dp/B00BLC2EH8/";
+		return "星の王子さま";
+		break
+
+		case '2_001':
 		link = "https://www.amazon.co.jp/gp/product/B00QWGZ1JG";
 		return "ブギーポップは笑わない";
 		break
 
-		case '1_003':
+		case '2_002':
 		link = "https://www.amazon.co.jp/gp/product/B00FZH3NR0";
 		return "この素晴らしい世界に祝福を！ | 第一巻";
 		break
 
-		case '1_004':
+		case '2_003':
 		link = "https://www.amazon.co.jp/gp/product/B00GZK5SCY";
 		return "この素晴らしい世界に祝福を！ | 第二巻";
 		break
 
-		case '1_005':
+		case '2_004':
 		link = "https://www.amazon.co.jp/gp/product/B00JMAIA6K";
 		return "この素晴らしい世界に祝福を！ | 第三巻";
 		break
 
-		case '2_001':
+		case '2_005':
+		link = "https://www.amazon.co.jp/gp/product/404912274X";
+		return "夜明けのブギーポップ";
+		break
+
+		case '2_006':
+		link = "https://www.amazon.co.jp/gp/product/B01DK0TRFK";
+		return "魔女の旅々(GAノベル) | 第一巻";
+		break
+
+		case '2_007':
+		link = "https://www.amazon.co.jp/gp/product/B00QWGZ0AG";
+		return "ブギーポップ・リターンズ VS　イマジネーターPart2";
+		break
+
+		case '2_008':
+		link = "https://www.amazon.co.jp/gp/product/B01HR6E2VM";
+		return "魔女の旅々(GAノベル) | 第二巻";
+		break
+
+		case '2_009':
 		link = "https://www.amazon.co.jp/gp/product/4061388126";
 		return "ダンガンロンパ/ゼロ| 第一巻(上)";
 		break
 
-		case '2_002':
+		case '2_0010':
 		link = "https://www.amazon.co.jp/gp/product/B00QWGYZY8";
 		return "ブギーポップ・リターンズ　VS　イマジネーターPart1";
 		break
 
 		case '3_001':
-		link = "https://www.amazon.co.jp/gp/product/404912274X";
-		return "夜明けのブギーポップ";
-		break
-
-		case '3_002':
 		link = "https://www.amazon.co.jp/gp/product/404727027X";
 		return "○X△べーす | 第一巻";
 		break
 
-		case '3_003':
-		link = "https://www.amazon.co.jp/gp/product/B00QWGZ0AG";
-		return "ブギーポップ・リターンズ VS　イマジネーターPart2";
-		break
-
-		case '3_004':
-		link = "https://www.amazon.co.jp/gp/product/B01DK0TRFK";
-		return "魔女の旅々(GAノベル) | 第一巻";
-		break
-
-		case '3_005':
-		link = "https://www.amazon.co.jp/gp/product/B01HR6E2VM";
-		return "魔女の旅々(GAノベル) | 第二巻";
+		case '3_002':
+		link = "https://www.amazon.co.jp/dp/B00DI8TF0W/";
+		return "電波女と青春男";
 		break
 	}
 }
