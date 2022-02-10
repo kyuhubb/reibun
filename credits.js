4497c39ee6a63d28ae3f3ab451041d7617c2deb9
Kyu <35692364+budouen@users.noreply.github.com>
2022-02-10 02:17:10 +0100

    Add files via upload

---
+++ credits.js
@@ -0,0 +1,98 @@
+function getC(credit){
+	switch(credit){
+		case '1_001':
+		link = "https://www.amazon.co.jp/gp/product/B077S47L7V";
+		return "魔女の家 エレンの日記 | 第一巻(漫画版)";
+		break
+
+		case '1_002':
+		link = "https://www.amazon.co.jp/dp/B0848V5LGR/";
+		return "星の子(朝日文庫)";
+		break
+
+		case '1_003':
+		link = "https://www.amazon.co.jp/dp/B00BLC2EH8/";
+		return "星の王子さま";
+		break
+
+		case '1_004':
+		link = "https://www.amazon.co.jp/dp/B06VS62J16";
+		return "勇者ですが異世界でエルフ嫁とピザ店始めます";
+		break
+
+		case '2_001':
+		link = "https://www.amazon.co.jp/gp/product/B00QWGZ1JG";
+		return "ブギーポップは笑わない";
+		break
+
+		case '2_002':
+		link = "https://www.amazon.co.jp/gp/product/B00FZH3NR0";
+		return "この素晴らしい世界に祝福を！ | 第一巻";
+		break
+
+		case '2_003':
+		link = "https://www.amazon.co.jp/gp/product/B00GZK5SCY";
+		return "この素晴らしい世界に祝福を！ | 第二巻";
+		break
+
+		case '2_004':
+		link = "https://www.amazon.co.jp/gp/product/B00JMAIA6K";
+		return "この素晴らしい世界に祝福を！ | 第三巻";
+		break
+
+		case '2_005':
+		link = "https://www.amazon.co.jp/gp/product/404912274X";
+		return "夜明けのブギーポップ";
+		break
+
+		case '2_006':
+		link = "https://www.amazon.co.jp/gp/product/B01DK0TRFK";
+		return "魔女の旅々(GAノベル) | 第一巻";
+		break
+
+		case '2_007':
+		link = "https://www.amazon.co.jp/gp/product/B00QWGZ0AG";
+		return "ブギーポップ・リターンズ VS　イマジネーターPart2";
+		break
+
+		case '2_008':
+		link = "https://www.amazon.co.jp/gp/product/B01HR6E2VM";
+		return "魔女の旅々(GAノベル) | 第二巻";
+		break
+
+		case '2_009':
+		link = "https://www.amazon.co.jp/gp/product/4061388126";
+		return "ダンガンロンパ/ゼロ| 第一巻(上)";
+		break
+
+		case '2_010':
+		link = "https://www.amazon.co.jp/gp/product/B00QWGYZY8";
+		return "ブギーポップ・リターンズ　VS　イマジネーターPart1";
+		break
+
+		case '2_011':
+		link = "https://www.amazon.co.jp/dp/B01MQKBDOA";
+		return "繰り巫女あやかし夜噺|第一巻";
+		break
+
+		case '2_012':
+		link = "https://www.amazon.co.jp/dp/B0093T5P5Y";
+		return "GOSICK|第一巻";
+		break
+
+		case '3_001':
+		link = "https://www.amazon.co.jp/gp/product/404727027X";
+		return "○X△べーす | 第一巻";
+		break
+
+		case '3_002':
+		link = "https://www.amazon.co.jp/dp/B00DI8TF0W/";
+		return "電波女と青春男";
+		break
+
+		case '3_003':
+		link = "https://www.amazon.co.jp/dp/B08BJ43WP8/";
+		return "Fate Zero | 第一巻";
+		break
+	}
+}
