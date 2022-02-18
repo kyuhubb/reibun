303986db8f8d884fcb86ca31a310ff2f81a38bb3
Kyu <35692364+budouen@users.noreply.github.com>
2022-02-18 20:05:58 +0100

    Add files via upload

--- credits.js
+++ credits.js
@@ -2,7 +2,7 @@ function getC(credit){
 	switch(credit){
 		case '1_001':
 		link = "https://www.amazon.co.jp/gp/product/B077S47L7V";
-		return "魔女の家 エレンの日記 | 第一巻(漫画版)";
+		return "魔女の家 エレンの日記 | 第１巻(漫画版)";
 		break
 
 		case '1_002':
@@ -27,17 +27,17 @@ function getC(credit){
 
 		case '2_002':
 		link = "https://www.amazon.co.jp/gp/product/B00FZH3NR0";
-		return "この素晴らしい世界に祝福を！ | 第一巻";
+		return "この素晴らしい世界に祝福を！ | 第１巻";
 		break
 
 		case '2_003':
 		link = "https://www.amazon.co.jp/gp/product/B00GZK5SCY";
-		return "この素晴らしい世界に祝福を！ | 第二巻";
+		return "この素晴らしい世界に祝福を！ | 第２巻";
 		break
 
 		case '2_004':
 		link = "https://www.amazon.co.jp/gp/product/B00JMAIA6K";
-		return "この素晴らしい世界に祝福を！ | 第三巻";
+		return "この素晴らしい世界に祝福を！ | 第３巻";
 		break
 
 		case '2_005':
@@ -47,7 +47,7 @@ function getC(credit){
 
 		case '2_006':
 		link = "https://www.amazon.co.jp/gp/product/B01DK0TRFK";
-		return "魔女の旅々(GAノベル) | 第一巻";
+		return "魔女の旅々(GAノベル) | 第１巻";
 		break
 
 		case '2_007':
@@ -57,12 +57,12 @@ function getC(credit){
 
 		case '2_008':
 		link = "https://www.amazon.co.jp/gp/product/B01HR6E2VM";
-		return "魔女の旅々(GAノベル) | 第二巻";
+		return "魔女の旅々(GAノベル) | 第２巻";
 		break
 
 		case '2_009':
 		link = "https://www.amazon.co.jp/gp/product/4061388126";
-		return "ダンガンロンパ/ゼロ| 第一巻(上)";
+		return "ダンガンロンパ/ゼロ | 第１巻(上)";
 		break
 
 		case '2_010':
@@ -72,27 +72,57 @@ function getC(credit){
 
 		case '2_011':
 		link = "https://www.amazon.co.jp/dp/B01MQKBDOA";
-		return "繰り巫女あやかし夜噺|第一巻";
+		return "繰り巫女あやかし夜噺 | 第１巻";
 		break
 
 		case '2_012':
 		link = "https://www.amazon.co.jp/dp/B0093T5P5Y";
-		return "GOSICK|第一巻";
+		return "GOSICK | 第１巻";
+		break
+
+		case '2_013':
+		link = "https://www.amazon.co.jp/dp/B009GPMJHC";
+		return "GOSICK | 第２巻";
+		break
+
+		case '2_014':
+		link = "https://www.amazon.co.jp/dp/B009GPMKEE";
+		return "GOSICK | 第３巻";
+		break
+		
+		case '2_015':
+		link = "https://www.amazon.co.jp/dp/4758047634";
+		return "吸血鬼に彼女役を頼んだ結果";
+		break
+
+		case '2_016':
+		link = "https://www.amazon.co.jp/dp/B009EN3M7C";
+		return "涼宮ハルヒ│第１巻";
+		break
+
+		case '2_017':
+		link = "https://www.amazon.co.jp/dp/B009EN3MNG";
+		return "涼宮ハルヒ│第２巻";
+		break
+
+		case '2_018':
+		link = "https://www.amazon.co.jp/dp/B009EN3N58";
+		return "涼宮ハルヒ│第３巻";
 		break
 
 		case '3_001':
 		link = "https://www.amazon.co.jp/gp/product/404727027X";
-		return "○X△べーす | 第一巻";
+		return "○X△べーす | 第１巻";
 		break
 
 		case '3_002':
 		link = "https://www.amazon.co.jp/dp/B00DI8TF0W/";
-		return "電波女と青春男";
+		return "電波女と青春男 │ 第１巻";
 		break
 
 		case '3_003':
 		link = "https://www.amazon.co.jp/dp/B08BJ43WP8/";
-		return "Fate Zero | 第一巻";
+		return "Fate Zero | 第１巻";
 		break
 	}
 }
